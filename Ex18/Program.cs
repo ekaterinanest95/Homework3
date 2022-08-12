52d8bf643e8772b358bfb6a8f189e956836ec477
Ekaterina <katya.kinal@gmail.com>
2022-08-12 13:35:04 +0300

    Ex18

---
+++ Ex18/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if((number % 100000) / 10000 == (number % 10) && (number % 10000) / 1000 == (number % 100) / 10)
+{
+    Console.WriteLine($"Число {number} является палиндромом");
+}
+else
+{
+    Console.WriteLine($"Число {number} не является палиндромом");
+}
+
+
